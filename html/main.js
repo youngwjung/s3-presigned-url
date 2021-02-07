f306261e88e3889e50839cf86bd009fd3aefb8e5
Young Woo Jung <youngwjung@gmail.com>
2021-02-07 18:41:16 +0900

    first commit

---
+++ html/main.js
@@ -0,0 +1,50 @@
+window.onload = function () {
+  fetch("http://BACKEND:5000", {
+    mode: "cors",
+  })
+    .then((res) => res.json())
+    .then((out) => {
+      console.log("Output: ", out);
+      document.getElementById("bucket_name").innerHTML += out.bucket_name;
+    })
+    .catch((err) => console.error(err));
+};
+
+const input = document.querySelector("input");
+document.getElementById("upload").addEventListener("click", upload);
+
+function upload() {
+  const files = input.files;
+  if (files.length === 0) {
+    alert("No files currently selected for upload");
+  } else {
+    for (const file of files) {
+      fetch("http://BACKEND:5000/upload", {
+        mode: "cors",
+        method: "POST",
+        body: JSON.stringify({
+          key: file.name,
+        }),
+      })
+        .then((res) => res.json())
+        .then((out) => {
+          const formData = new FormData();
+
+          Object.keys(out.fields).forEach((key) => {
+            formData.append(key, out.fields[key]);
+          });
+
+          formData.append("file", file);
+
+          fetch(out.url, {
+            mode: "cors",
+            method: "POST",
+            body: formData,
+          }).then((response) => {
+            console.log(response);
+          });
+        })
+        .catch((err) => console.error(err));
+    }
+  }
+}
---
+++ main.py
@@ -0,0 +1,42 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import boto3
+import requests
+
+
+region = requests.get('http://169.254.169.254/latest/meta-data/placement/region').text
+bucket = BUCKET_NAME
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class S3Object(BaseModel):
+    file_name: str
+
+
+@app.get("/")
+def root():
+    return {"bucket_name": bucket}
+
+
+@app.post("/upload")
+def upload(object: S3Object):
+    ## Boto3를 이용해서 파일 업로드에 사용할 미리 서명된 URL을 받아서 반환하세요. SDK에서 반환되는 응답을 그대로 반환하면 돱니다.
+
+    return response
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
